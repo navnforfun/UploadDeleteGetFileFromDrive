86f95f6e1783c2e8ddbf7d7e62b93ce59b677e12
NgocAnh <navnforfun@gmail.com>
2024-05-26 22:19:59 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Text.Json;
+using UploadDrive;
+
+string folderId = "1jWgakpLKDVNOcVyojmjeuhZwAa5yyMAm";
+string folderUploadPath = "D:\\YTB\\c1_UseDriveAPI\\Test\\I save your life.txt";
+Extend.UploadFileToGoogleDrive(folderId,folderUploadPath);
+Console.WriteLine("====");
+var listFile = Extend.GetLisFiles(folderId);
+foreach (var file in listFile)
+{
+    Console.WriteLine($"{file.Name} {file.Id}");
+}
+Extend.DeleteFile(folderId);
